4f52f9e54f0b93b8cc128c689de065ccffcc5c4c
ramasatriana <ramasatri49@gmail.com>
2025-03-04 16:27:12 +0700

    Initial commit - push project to GitHub

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  // Konfigurasi tambahan lainnya
 };
 
 export default nextConfig;
