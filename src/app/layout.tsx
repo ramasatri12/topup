73435cc14fc91e87291fd58fccb05f58dafa7599
ramasatriana <ramasatri49@gmail.com>
2025-03-04 17:05:21 +0700

    setup signup and signup photo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,12 @@ import "../styles/checkout.css";
 import "../styles/complete-checkout.css";
 import "../styles/sign-in.css";
 import "../styles/404-not-found.css";
+import "../styles/sign-up.css";
+import "../styles/sign-up-success.css";
+import "../styles/sign-up-photo.css";
+import "../styles/sign-up-photo-success.css";
+import "../styles/transactions.css";
+import "../styles/transactions-details.css";
 
 
 
