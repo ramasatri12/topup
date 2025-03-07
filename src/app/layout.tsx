7d34f76cc7298c0d18bd47c948f92bb377733380
ramasatriana <ramasatri49@gmail.com>
2025-03-07 16:57:52 +0700

    refactor login navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,16 @@ import "../styles/checkout.css";
 import "../styles/complete-checkout.css";
 import "../styles/sign-in.css";
 import "../styles/404-not-found.css";
+import "../styles/sidebar.css";
 import "../styles/sign-up.css";
 import "../styles/sign-up-success.css";
 import "../styles/sign-up-photo.css";
 import "../styles/sign-up-photo-success.css";
 import "../styles/transactions.css";
-import "../styles/transactions-details.css";
+import "../styles/transactions-detail.css";
+import "../styles/navbar-log-in.css";
+import "../styles/overview.css";
+import "../styles/edit-profile.css";
 
 
 
